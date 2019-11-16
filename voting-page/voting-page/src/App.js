5efb270eae3f9fb8c9abbc35b445a5a69ac86909
Kevin He <yc4he@uwaterloo.ca>
2019-11-15 21:41:21 -0500

    modified voting page

--- voting-page/voting-page/src/App.js
+++ voting-page/voting-page/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <header>Waterloo </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.submitVote = this.submitVote.bind(this);
+  }
+
+  submitVote = () => {
+    alert(1);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>Waterloo Idol Final 2019 Voting Page</header>
+        {/* <body> */}
+          <div className="row">
+            <button onClick={this.submitVote} className="vote_button">Vote: 1</button>
+          </div>
+        {/* </body> */}
+      </div>
+    );
+  }
 }
 
 export default App;
